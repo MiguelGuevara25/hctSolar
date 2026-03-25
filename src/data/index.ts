ef58cb6a7af53616189170e1b77d916c2f4d6131
Encode-dev <encode_dev@hotmail.com>
2026-03-25 11:42:36 -0500

    lauch project

---
+++ src/data/index.ts
@@ -0,0 +1,8 @@
+export const secciones = [
+  { id: 1, nombre: "Tecnología", slug: "#tecnologia" },
+  { id: 2, nombre: "Aplicaciones", slug: "#aplicaciones" },
+  { id: 3, nombre: "Beneficios", slug: "#beneficios" },
+  { id: 4, nombre: "Funcionamiento", slug: "#funcionamiento" },
+  { id: 5, nombre: "Escalabilidad", slug: "#escalabilidad" },
+  { id: 6, nombre: "Impacto", slug: "#impacto" },
+];
