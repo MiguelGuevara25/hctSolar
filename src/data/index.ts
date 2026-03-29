a99a82191dab7d620b7bb9b36c23f03e174b1edd
Encode-dev <encode_dev@hotmail.com>
2026-03-28 22:29:09 -0500

    feat: section partner completed

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,4 +1,6 @@
-export const secciones = [
+import type { PartnerLogo, Secciones } from "../interfaces";
+
+export const secciones: Secciones[] = [
   {
     id: 1,
     nombre: "Tecnología",
@@ -28,5 +30,48 @@ export const secciones = [
     id: 6,
     nombre: "Impacto",
     slug: "#impacto",
+  },
+];
+
+export const partnersLogos: PartnerLogo[] = [
+  {
+    id: 1,
+    src: "/partners/marcas_logo1.png",
+    alt: "Partner 1",
+  },
+  {
+    id: 2,
+    src: "/partners/marcas_logo2.png",
+    alt: "Partner 2",
+  },
+  {
+    id: 3,
+    src: "/partners/marcas_logo3.png",
+    alt: "Partner 3",
+  },
+  {
+    id: 4,
+    src: "/partners/marcas_logo4.png",
+    alt: "Partner 4",
+  },
+  {
+    id: 5,
+    src: "/partners/marcas_logo5.png",
+    alt: "Partner 5",
+  },
+  {
+    id: 6,
+    src: "/partners/marcas_logo6.png",
+    alt: "Partner 6",
+  },
+  {
+    id: 7,
+    src: "/partners/marcas_logo7.png",
+    alt: "Partner 7",
+  },
+  {
+    id: 8,
+    src: "/partners/marcas_logo8.png",
+    alt: "Partner 8",
   },
 ];
---
+++ src/interfaces/index.ts
@@ -0,0 +1,11 @@
+export interface Secciones {
+  id: number;
+  nombre: string;
+  slug: string;
+}
+
+export interface PartnerLogo {
+  id: number;
+  src: string;
+  alt: string;
+}
